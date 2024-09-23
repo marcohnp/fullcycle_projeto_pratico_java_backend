06fc6103f60ba79a33a64ffbd31812badee2838a
Marco Henrique Nunes Pereira <marcohnp@hotmail.com>
2024-09-23 20:30:52 -0300

    Add Spring and dependencies of infra

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/App.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/App.java
@@ -1,10 +1,17 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
-import com.fullcycle.admin.catalogo.application.UseCase;
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
+@SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
 
         System.out.printf("Hello and welcome!");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/AppTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/AppTest.java
@@ -1,6 +1,7 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +9,7 @@ class AppTest {
 
     @Test
     void testApp() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         assertNotNull(new App());
         App.main(new String[]{});
     }
